69f6242e93075a691029592fa9250b912dea1417
albertoicdiaz <albertoicdiaz@gmail.com>
2019-08-08 11:50:03 -0400

    parte 1

---
+++ Proyecto de Título/Assets/Act4/backgroundLoop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundLoop : MonoBehaviour
+{
+    [SerializeField]
+    private Transform centerBackground;
+
+    void Update ()
+    {
+        if (transform.position.y >= centerBackground.position.y+ 5)
+            centerBackground.position = new Vector2(centerBackground.position.x, transform.position.y + 5f);
+        else if (transform.position.y <= centerBackground.position.y - -5f)
+            centerBackground.position = new Vector2 (centerBackground.position.x, transform.position.y - -5f);
+    }
+}
