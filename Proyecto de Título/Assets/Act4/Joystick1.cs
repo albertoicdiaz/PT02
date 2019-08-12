582ec92c4889c89c4b1a1f1ba65558008d8eb744
albertoicdiaz <albertoicdiaz@gmail.com>
2019-08-12 13:26:44 -0400

    cambios actividad 4

---
+++ Proyecto de Título/Assets/Act4/Joystick1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystick1 : MonoBehaviour {
+    public Transform player;
+    public float speed = 5.0f;
+
+	// Update is called once per frame
+	void Update () 
+    {
+        moveCharacter (new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+	}
+	void moveCharacter(Vector2 direction){
+        player.Translate(direction * speed * Time.deltaTime);
+    }
+}
